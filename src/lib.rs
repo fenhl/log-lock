78d402e141a867e9042ebd77dbadd3abfa2bf17b
Fenhl <fenhl@fenhl.net>
2026-06-10 15:44:22 +0000

    Move lint config to Cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,3 @@
-#![allow(unused_qualifications)]
-#![deny(rust_2018_idioms, unused, unused_crate_dependencies, unused_import_braces, unused_lifetimes, unused_qualifications, warnings)]
-#![forbid(unsafe_code)]
-
 use {
     std::sync::Arc,
     dashmap::DashMap,
